d2959a313e1d7802ce9754103c4e04c47d21048c
Mark Steadman <marksteadman@me.com>
2014-04-22 22:41:09 +0100

    v1.0 release

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from distutils.core import setup
+from os import path
 
 setup(
 	name = 'bambu-ajax',
@@ -9,9 +10,20 @@ setup(
 	author_email = 'mark@steadman.io',
 	url = 'https://github.com/iamsteadman/bambu-ajax',
 	namespace_packages = ['bambu'],
+	long_description = open(path.join(path.dirname(__file__), 'README')).read(),
 	packages = [
-		'bambu.ajax'
+		'bambu.ajax',
+		'bambu.ajax.templatetags',
+		'bambu.ajax.test'
 	],
+	package_data = {
+		'bambu.ajax': [
+			'templates/ajax/utils.js',
+			'templates/blog/*.html',
+			'templates/search/indexes/blog/*.txt',
+			'static/blog/*.js'
+		]
+	},
 	classifiers = [
 		'Development Status :: 4 - Beta',
 		'Environment :: Web Environment',
